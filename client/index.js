4171f73a83c9356dee64bc2fa090d73e9c8dd88f
= <rob.sykes@sykes-technologies.com>
2016-02-16 21:57:25 +0000

    Added new ignore to gitignore.

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@ requirejs.config({
     baseUrl: 'js',
     paths: {
         jquery: 'https://code.jquery.com/jquery-2.2.0',
-        config: '/client/config/config',
-        routes: '/client/config/routes'
+        config: '/config/config',
+        routes: '/config/routes'
     }
 })
 
