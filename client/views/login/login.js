880faba41493ae6cf78e7e07c6b83f06e00f7f38
= <rob.sykes@sykes-technologies.com>
2016-02-16 23:07:46 +0000

    Client Side login filler.

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,8 +1,21 @@
-define(["jquery", "app"], function($, App) {
+define(["jquery", "app", "auth"], function($, App, Auth) {
     
-    // Go to register page
-    console.log("hello world!");
-    
-    
-    
+    // Handle login button click.
+	$("#loginBtn").click(function() { 
+
+		var credentials = { 
+			Username: $("#username").val(),
+			Password: $("#password").val()
+		};
+
+		// Login via the authentication system.
+		Auth.login(credentials, function(result) { 
+			console.log(result);
+		}, 
+		function() { 
+			console.log("Failed to login! Invalid Credentials!");
+		});
+
+	});
+   
 });
