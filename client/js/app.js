2da047057d4eb061d0759f3feb92fec1013d78c5
= <rob.sykes@sykes-technologies.com>
2016-02-17 19:22:56 +0000

    Fixed issues with resource manifest not being loaded into the application. More specifically the localisation phrases.

--- client/js/app.js
+++ client/js/app.js
@@ -6,14 +6,18 @@ define(["jquery", "config", "routes"], function($, config, Routes) {
 
 	return {
 
-		setAssets: function(preloadAssets) {
-			assets = preloadAssets;
+		// Initializes the application
+		init: function(assetsList) {
+			assets = assetsList;
+			this.routeTo("login");
 		},
 
+		// Grabs the site assets.
 		getAssets: function() {
 			return assets;
 		},
 
+		// Gets a particular phrase from the locale.
 		getPhrase: function(phrase) {
 
 			var phrase = assets.getResult(config.Language)[phrase];
@@ -24,6 +28,7 @@ define(["jquery", "config", "routes"], function($, config, Routes) {
 			else return phrase;
 		},
 
+		// shows an alert on the screen.
 		showAlert: function(phrase, showOK, optionalText, callback) {
 
 			var phrase = this.getPhrase(phrase);
@@ -41,6 +46,8 @@ define(["jquery", "config", "routes"], function($, config, Routes) {
 			}
 		},
         
+        // Routes to a new page. Defined
+        // in the routes file.
         routeTo: function(route) {
             if ( !route ) {
                 throw "No route given!";
