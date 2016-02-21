541d6afc44f49a90905227048200660a607d912c
= <rob.sykes@sykes-technologies.com>
2016-02-21 09:47:52 +0000

    - Added repositories and data context to server to work better with services. - Removed obselete connections object. - Plummed in the auth validate event so that the user is checked and confirmed before actually logging in.

--- server/src/server.js
+++ server/src/server.js
@@ -3,16 +3,17 @@
 var url  = require('url');
 var http = require('http').createServer();
 var Config = require('./config.js');
-var Connection = require('./connection.js');
-var Events = require('./events.js');
+var Events = require('./event_registry.js');
 
-var Server = function() {
-	this.connections = [];
+var Server = function(dbConnection) 
+{
 	this.io = null;
+	this.dbConnection = dbConnection; 
 }
 
 // Create a new server instance.
-Server.prototype.create = function() {
+Server.prototype.create = function() 
+{
 	var self = this;
 
 	this.io = require('socket.io')();
@@ -22,26 +23,38 @@ Server.prototype.create = function() {
 }
 
 // Called when a connection is made to the server.
-Server.prototype.onConnect = function(callback) {
+Server.prototype.onConnect = function(callback) 
+{
 	this.connection_callback = callback;
 }
 
 // Called when a connection is disconnected from the database.
-Server.prototype.onDisconnect = function(callback) { 
+Server.prototype.onDisconnect = function(callback) 
+{ 
 	this.disconnect_callback = callback;
+}
+
+Server.prototype.send = function(tag, data) 
+{
+	this.io.emit(tag, data);
 }
 
 // Listens for incoming events and queries
 // the event registry for the function call.
-Server.prototype.listen = function() {
+Server.prototype.listen = function() 
+{
 
-	this.io.on('connection', function(socket) {
-		socket.on('*', function(event) {
+	var self = this;
+
+	this.io.on('connection', function(socket) 
+	{
+		socket.on('*', function(event) 
+		{
 			
 			var eventKey = event.data[0];
 			var params = event.data[1];
 
-			Events.EventRegistry[eventKey].callback(params);
+			Events.EventRegistry[eventKey].callback(params, self);
 			
 		});
 	});
--- server/src/services/auth.js
+++ server/src/services/auth.js
@@ -1,12 +1,33 @@
+var Context = require('../dataContext.js');
+
 module.exports = {
 
-	validate: function(credentials) {
+	validate: function(credentials, server) 
+	{
 
-		if ( !credentials ) {
+		if ( !credentials ) 
+		{
 			throw "No credentials given!";
 		}
-		
-		console.log(credentials.username + " has logged in.");
+
+		console.log(credentials.Username + " has attempted to log in.");
+
+		// Check the credentials against he database.
+		Context.get("users").findByCredentials(credentials,
+			function(result) 
+			{
+
+				if ( result != undefined ) 
+				{
+					server.send("auth_validate", result);
+					console.log(credentials.Username + " has been validated.");
+					return;
+				}
+
+				server.send("auth_validate", false);
+				console.log(credentials.Username + " has been rejected. Incorrect Credentials!");
+
+			});
 	}
 
 };
